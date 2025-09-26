8224f310a3dc6a9b55ba812154b3da5e4cc1c1fb
AlejandroRodrigo18 <alejandro.rodrigo4@educa.madrid.org>
2025-09-26 09:30:22 +0200

    Subida del Programa 01

---
+++ PEP15/U1/programas_02/programa_01.py
@@ -0,0 +1,10 @@
+
+entero = 12
+x= 3.14159
+y= (10==11)
+cadena = "Desarrollo de Programas en Python"
+
+print("La variable",entero, "es de tipo:",type(entero))
+print("La variable",x , "es de tipo:",type(x))
+print("La variable", y , "es de tipo:",type(y) )
+print("La variable '", cadena, "' es de tipo:",type(cadena))
