f73e7404640e2d6a1a4ec71266731e7944f7ce00
AlejandroRodrigo18 <alejandro.rodrigo4@educa.madrid.org>
2025-10-09 10:19:02 +0200

    Add files via upload

---
+++ PEP15/U3/ Programas_01_Funciones/programa02.py
@@ -0,0 +1,9 @@
+def entrada():
+    nombre = input("Escribe tu nombre: ")
+    apellido1 = input("Escribe tu primer apellido: ")
+    apellido2 = input("Escribe tu segundo apellido: ")
+    curso = "2ºDAW"
+
+    print(nombre, apellido1, apellido2, curso)
+
+entrada()
